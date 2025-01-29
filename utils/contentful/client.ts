7dc3c71477eeac9c1cdc72b69e4f85ce1a24c3d8
Mahit Mehta <mahit.py@gmail.com>
2025-01-29 16:21:35 -0500

    tokens

--- utils/contentful/client.ts
+++ utils/contentful/client.ts
@@ -1,12 +1,16 @@
+const CONTENTFUL_PREVIEW_ACCESS_TOKEN = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+
 export async function fetchContentful<T>(
 	query: string,
 	variables = {},
 	isDraftModeEnabled = false,
 ): Promise<T> {
-	const CONTENTFUL_ACCESS_TOKEN =
+	console.log("prod:", CONTENTFUL_ACCESS_TOKEN);
+	console.log("preview:", CONTENTFUL_PREVIEW_ACCESS_TOKEN);
+	const access_token =
 		process.env.NODE_ENV !== "production" || isDraftModeEnabled
-			? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-			: process.env.CONTENTFUL_ACCESS_TOKEN;
+			? CONTENTFUL_PREVIEW_ACCESS_TOKEN || CONTENTFUL_ACCESS_TOKEN
 
 	return fetch(
 		`https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
@@ -14,7 +18,7 @@ export async function fetchContentful<T>(
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
-				authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
+				authorization: `Bearer ${access_token}`,
 			},
 			body: JSON.stringify({ query, variables }),
 		},
