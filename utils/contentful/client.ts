50c173bb1fcf38e558ba04af40b60dc39820f499
Mahit Mehta <mahit.py@gmail.com>
2025-01-29 13:14:19 -0500

    Updated Access Token Config

--- utils/contentful/client.ts
+++ utils/contentful/client.ts
@@ -1,6 +1,7 @@
 const CONTENTFUL_ACCESS_TOKEN =
 	process.env.NODE_ENV === "production"
-		? process.env.CONTENTFUL_ACCESS_TOKEN
+		? process.env.CONTENTFUL_ACCESS_TOKEN ||
+			process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
 		: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
 
 export async function fetchContentful<T>(
